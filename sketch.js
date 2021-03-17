72d94bb09ed62f8ead81d1acc83d3291f2a6b6a1
ayanderie <79267936+ayanderie@users.noreply.github.com>
2021-03-17 12:03:21 -0400

    Create sketch.js
    yesss

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(2000, 2000);
+}
+
+function draw() {
+  background(102,178,255);
+  fill(255);
+
+  textFont("Impact", 50);
+  text("Everyone should have access to clean water and sanitation",380, 60);
+}
