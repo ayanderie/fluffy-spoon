f5991590be58a6abfb00ab2e0381d1ba3b129b6b
ayanderie <79267936+ayanderie@users.noreply.github.com>
2021-03-17 13:01:03 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,9 +3,16 @@ function setup() {
 }
 
 function draw() {
-  background(102,178,255);
+  background(102, 178, 255);
   fill(255);
 
   textFont("Impact", 50);
-  text("Everyone should have access to clean water and sanitation",380, 60);
+  text("Everyone should have access to clean water and sanitation", 380, 65);
+  noStroke()
+  rect(44, 99, 230,230);
+  fill(102, 178, 255);
+  textSize(65);
+  text("1 in 10",60,159);
+  textSize(25);
+  text("people live without", 45, 200);
 }
