889437fec3d6dc32fe44514f1156fe288aa68f22
ayanderie <79267936+ayanderie@users.noreply.github.com>
2021-03-19 10:11:34 -0400

    Update sketch.js
    updating thidsss

--- sketch.js
+++ sketch.js
@@ -1,32 +1,36 @@
 let img;
 
-function preload () {
-  img = loadImage ('water_stateOfPlay.jpg');
+function preload() {
+  img = loadImage('water_stateOfPlay.jpg');
 }
 
 function setup() {
-  createCanvas(2000, 2000);
+  createCanvas(2000, 1000);
 }
 
 function draw() {
-  background(102, 178, 255);
-  image (img,500,150,450,380);
+  background(43, 50, 76);
+
+  image(img, 900, 150, 550, 400);
   fill(255);
 
   textFont("Impact", 50);
-  text("｡･:*˚:✧｡ Not everyone has access to clean water and sanitation｡･:*˚:✧｡", 380, 65);
-  text ("Let's talk about it!!!",900,130);
 
+  text("Not everyone has access to clean water and sanitation", 450, 85);
+  text(" ｡｡･:*˚✧｡Let's talk about it ｡｡･:*˚:✧｡", 600, 130);
   noStroke()
-  rect(44, 99, 200, 100);
-  fill(102, 178, 255);
-  textSize(75);
-  text("1 in 3", 70, 175);
+  fill(85, 118, 185);
+  rect(430, 175, 280, 130);
+  fill(43, 50, 76);
+  textSize(95);
+  text("1 in 3", 465, 280);
 
   textSize(25);
   fill(255);
-  textFont("times new roman");
-  text("people globally do not have", 27, 240);
-  text("access to safe drinking water", 20, 270);
+  textFont("times new roman", 30);
+  text("people globally do not have", 400, 340);
+  text("access to safe drinking water", 400, 370);
+
+
 
 }
